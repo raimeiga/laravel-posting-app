dd88d939ab3fc9b468ea275dec7c1d1302efb068
raimeiga <yusuke612@hotmail.com>
2023-10-05 08:48:30 +0900

    8.4 Postコントローラの中身を確認　namespaceとuse宣言の説明追記

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,14 @@
+<?php
+// ↓ namespace（名前空間）= クラスの住所を示す
+namespace App\Http\Controllers;
+
+/* ↓use宣言=このファイルではこのクラスを使います」と宣言
+    このファイルではIlluminate\Httpフォルダの中にあるRequestクラスを使うよ.と宣言
+    宣言により、そのファイル内ではRequestと記述するだけでRequestクラスを呼び出せるようになる*/
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    //
+}
