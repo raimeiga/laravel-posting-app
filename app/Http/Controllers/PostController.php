4cb9a8f9c1225e7d7aadb53d01a97458fbc2cbce
raimeiga <yusuke612@hotmail.com>
2023-10-05 16:18:00 +0900

    コントローラのファイルであることを冒頭に追記

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,3 +1,4 @@
+<!-- このファイルはコントローラのファイル -->
 <?php
 // ↓ namespace（名前空間）= クラスの住所を示す
 namespace App\Http\Controllers;
@@ -12,5 +13,10 @@ class PostController extends Controller {
    // 一覧ページ
    public function index() {        
     return view('posts.index');
-   }
+   } 
 }
+  /* ↑ view()ヘルパー=ビューを表示するためのもの。
+　　view('posts.index')のように、表示したいビューを引数として指定する
+　　postsフォルダのindex.blade.phpファイル（ビュー画面のファイル）
+　　resources/views/posts/index.blade.phpというURLだが、
+　　resources/viewsを省略し、フォルダ名.ファイル名（.blade.phpは不要）と記述*/
