c734c9727c862b2fc8df00f37e847fc02ff063e5
raimeiga <yusuke612@hotmail.com>
2023-10-05 09:16:21 +0900

    8.5 indexアクションを作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,9 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class PostController extends Controller
-{
-    //
+class PostController extends Controller {
+   // 一覧ページ
+   public function index() {        
+    return view('posts.index');
+   }
 }
