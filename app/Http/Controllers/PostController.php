a362a9adf5dd5de63b14b8d810bb3cd98522edcc
raimeiga <yusuke612@hotmail.com>
2023-10-06 23:24:43 +0900

    コメントの場所を修正

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,23 +1,18 @@
-<!-- このファイルはコントローラのファイル。↓のview('posts.index')により、
-resources/views/posts/index.blade.phpファイルを読み込む -->
-<?php  // ↓ namespace（名前空間）= クラスの住所を示す
-namespace App\Http\Controllers; 
+<?php   //このファイルはコントローラのファイル。
+namespace App\Http\Controllers;   // ← namespace（名前空間）はクラスの住所を示す
 
-/* ↓use宣言=このファイルではこのクラスを使います」と宣言
-    このファイルではIlluminate\Httpフォルダの中にあるRequestクラスを使うよ.と宣言
-    宣言により、そのファイル内ではRequestと記述するだけでRequestクラスを呼び出せるようになる*/
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+/* ↑ use宣言=このファイルではこのクラスを使います」と宣言
+  このファイルではIlluminate\Httpフォルダの中にあるRequestクラスを使うよ.と宣言
+  宣言により、そのファイル内ではRequestと記述するだけでRequestクラスを呼び出せるようになる*/
 class PostController extends Controller {
    // 一覧ページ
    public function index() {        
     return view('posts.index');
    } 
 }
-  /* ↑ view()ヘルパー=ビューを表示するためのもの。
-　　view('posts.index')のように、表示したいビューを引数として指定する
-　　postsフォルダのindex.blade.phpファイル（ビュー画面のファイル）
-　　resources/views/posts/index.blade.phpというURLだが、
-　　resources/viewsを省略し、フォルダ名.ファイル名（.blade.phpは不要）と記述*/
-
+   /* ↑ view()ヘルパー=ビューを表示するためのもの。
+　　  view('posts.index')のように、表示したいビューを引数として指定する
+　　  postsフォルダのindex.blade.phpファイル（ビュー画面のファイル）
+　　  resources/views/posts/index.blade.phpというURLだが、
+　　  resources/viewsを省略し、フォルダ名.ファイル名（.blade.phpは不要）と記述*/
