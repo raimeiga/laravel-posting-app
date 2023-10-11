a159ad042beabfdd854b2643842cdd5c56f1fe16
raimeiga <yusuke612@hotmail.com>
2023-10-11 09:58:02 +0900

    16.7 Bootstrapをビルド

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,6 +6,8 @@
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>新規投稿</title>    
+
+     @vite(['resources/js/app.js'])
  </head>
  
  <body>
