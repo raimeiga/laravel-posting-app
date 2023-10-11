420259f310cb17b0f62f2a9f689f12bc3c844dfe
raimeiga <yusuke612@hotmail.com>
2023-10-11 09:58:08 +0900

    16.7 Bootstrapをビルド

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -4,7 +4,9 @@
  <head>
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>投稿編集</title>    
+     <title>投稿編集</title>   
+     
+     @vite(['resources/js/app.js'])
  </head>
  
  <body>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,6 +7,7 @@
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>投稿詳細</title>
      
+     @vite(['resources/js/app.js'])
  </head>
  
  <body>
