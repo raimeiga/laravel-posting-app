69fc21b127c730a4f58a3ec9cb6142352312225d
raimeiga <yusuke612@hotmail.com>
2023-10-11 11:25:08 +0900

    16.8 各ビューを編集してBootstrapのclassを追加

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -10,11 +10,11 @@
      @vite(['resources/js/app.js'])
  </head>
  
- <body>
+ <body style="padding: 60px 0;">
      <header>
-         <nav>
-             <div>                
-                 <a href="{{ route('posts.index') }}">投稿アプリ</a>    <!-- index = 投稿一覧ページ -->      
+        <nav class="navbar navbar-light bg-light fixed-top" style="height: 60px;">
+            <div class="container">                 
+                <a href="{{ route('posts.index') }}" class="navbar-brand">投稿アプリ</a>    <!-- index = 投稿一覧ページ -->      
              </div> 
                         <!-- ↑ route()ヘルパー＝名前付きルートを呼び出すヘルパー
                         例）<a href="{{ route('posts.index') }}">投稿アプリ</a>のように書く
@@ -25,10 +25,10 @@
  
      <main>
          <article>
-             <div>                
-                 <h1>新規投稿</h1>   
+            <div class="container">               
+                <h1 class="fs-2 my-3">新規投稿</h1>    
                   @if ($errors->any())  <!--　←　エラーメッセージの表示機能 -->
-                     <div>
+                     <div class="alert alert-danger">
                          <ul>
                              @foreach ($errors->all() as $error)
                                  <li>{{ $error }}</li>
@@ -37,8 +37,8 @@
                      </div>
                   @endif
                  
-                 <div>
-                     <a href="{{ route('posts.index') }}">&lt; 戻る</a>   <!-- index = 投稿一覧ページ -->                               
+                  <div class="mb-2">
+                  <a href="{{ route('posts.index') }}" class="text-decoration-none">&lt; 戻る</a>   <!-- index = 投稿一覧ページ -->                               
                  </div>
  
                  <form action="{{ route('posts.store') }}" method="post"> 
@@ -46,25 +46,25 @@
                             Laravelでフォームを作成するときは必ず@csrfを記述。
                             Laravelではformタグ内にcsrfと記述するだけでCSRF対策が可能 -->
                       @csrf 
-                     <div>
+                      <div class="form-group mb-3">
                          <label for="title">タイトル</label>
-                         <input type="text" name="title" value="{{ old('title') }}">
+                         <input type="text" class="form-control" name="title" value="{{ old('title') }}">
                      </div>
                      <!-- ↑ ↓ old()ヘルパー=引数にフォームのname属性の値を指定することで、そのフォームの直前の
                      　　　　 入力値を取得直前の入力値が存在しない場合はNULLを返すので、エラー時以外は通常どお
                               り初期値は空欄になる -->
-                     <div>
+                     <div class="form-group mb-3">
                          <label for="content">本文</label>
-                         <textarea name="content">{{ old('content') }}</textarea>
+                         <textarea class="form-control" name="content">{{ old('content') }}</textarea>
                      </div>
-                     <button type="submit">投稿</button>
+                     <button type="submit" class="btn btn-outline-primary">投稿</button>
                  </form>
              </div>
          </article>
      </main>
  
-     <footer>        
-         <p>&copy; 投稿アプリ All rights reserved.</p>
+     <footer class="d-flex justify-content-center align-items-center bg-light fixed-bottom" style="height: 60px;">         
+        <p class="text-muted small mb-0">&copy; 投稿アプリ All rights reserved.</p>
      </footer>
  </body>
  
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -9,21 +9,21 @@
      @vite(['resources/js/app.js'])
  </head>
  
- <body>
+ <body style="padding: 60px 0;">
      <header>
-         <nav>
-             <div>                
-                 <a href="{{ route('posts.index') }}">投稿アプリ</a>          
+        <nav class="navbar navbar-light bg-light fixed-top" style="height: 60px;">
+            <div class="container">                
+                <a href="{{ route('posts.index') }}" class="navbar-brand">投稿アプリ</a>         
              </div>
          </nav>
      </header>
  
      <main>
          <article>
-             <div>                
-                 <h1>投稿編集</h1>   
+            <div class="container">                
+                <h1 class="fs-2 my-3">投稿編集</h1>  
                     @if ($errors->any())   <!--　←　エラーメッセージの表示機能 -->
-                       <div>
+                        <div class="alert alert-danger">
                            <ul>
                                @foreach ($errors->all() as $error)
                                    <li>{{ $error }}</li>
@@ -32,8 +32,8 @@
                        </div>
                    @endif
                                  
-                 <div>
-                     <a href="{{ route('posts.index') }}">&lt; 戻る</a>                                  
+                 <div class="mb-2">
+                    <a href="{{ route('posts.index') }}" class="text-decoration-none">&lt; 戻る</a>                                  
                  </div>
                  
                  <!-- ↓ formタグのaction属性にはupdateアクションへのルートを指定し、
@@ -43,27 +43,27 @@
                      @method('patch') 
                      <!-- ↑ HTMLのフォームはGETとPOST以外のHTTPリクエストメソッドをサポートしていないため、 
                             formタグ内にatマークmethod('patch')と書き、GETとPOST以外のHTTPリクエストメソッドが使用可能にする -->
-                     <div>
+                     <div class="form-group mb-3">
                          <label for="title">タイトル</label>
-                         <input type="text" name="title" value="{{ old('title', $post->title) }}">
+                         <input type="text" class="form-control" name="title" value="{{ old('title', $post->title) }}">
                      </div>
                             <!-- ↑ ↓ old()ヘルパー=引数にフォームのname属性の値を指定することで、そのフォームの直前の入力値を
                             取得直前の入力値が存在しない場合はNULLを返すので、エラー時以外は通常どおり初期値は空欄になるらしい。
                             しかし、 投稿編集ページは、すでに初期値が設定されているので、第2引数に「直前の入力値が存在しない場
                             合の初期値」、つまり「エラー時以外の通常の初期値」を指定することで、NULLの代わりに第2引数の値を返し
                             てくれる。。。ということらしい（意味不明）。-->
-                     <div>
+                     <div class="form-group mb-3">
                          <label for="content">本文</label>
-                         <textarea name="content">{{ old('content', $post->content) }}</textarea>
+                         <textarea class="form-control" name="content">{{ old('content', $post->content) }}</textarea> 
                      </div>
-                     <button type="submit">更新</button>
+                     <button type="submit" class="btn btn-outline-primary">更新</button>
                  </form>
              </div>
          </article>
      </main>
  
-     <footer>        
-         <p>&copy; 投稿アプリ All rights reserved.</p>
+     <footer class="d-flex justify-content-center align-items-center bg-light fixed-bottom" style="height: 60px;">
+        <p class="text-muted small mb-0">&copy; 投稿アプリ All rights reserved.</p>
      </footer>
  </body>
  
